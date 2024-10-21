3f963f154e81b58d9ba7fdcc773a860cf6a080c5
Peter Marshall <petms@proton.me>
2024-10-21 10:21:04 -0400

    Fix vendor string for Surface Pro 9

--- module/surface_gpe.c
+++ module/surface_gpe.c
@@ -119,7 +119,7 @@ static const struct dmi_system_id dmi_lid_device_table[] = {
 		 */
 		.ident = "Surface Pro 9",
 		.matches = {
-			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Microsoft Corporation"),
+			DMI_EXACT_MATCH(DMI_SYS_VENDOR, " Microsoft Corporation"),
 			DMI_EXACT_MATCH(DMI_PRODUCT_SKU, "Surface_Pro_9_2038"),
 		},
 		.driver_data = (void *)lid_device_props_l52,
